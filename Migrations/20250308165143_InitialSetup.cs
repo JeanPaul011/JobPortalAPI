6841d5d9d75127d24de11a82f54eac6e3ff488e8
JeanPaul011 <w2058402@westminster.ac.uk>
2025-03-10 12:55:40 +0000

    Temporarily adding ignored files for transfer

---
+++ Migrations/20250308165143_InitialSetup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobPortalAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Jobs_AspNetUsers_RecruiterId",
+                table: "Jobs");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Jobs_AspNetUsers_RecruiterId",
+                table: "Jobs",
+                column: "RecruiterId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Jobs_AspNetUsers_RecruiterId",
+                table: "Jobs");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Jobs_AspNetUsers_RecruiterId",
+                table: "Jobs",
+                column: "RecruiterId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
